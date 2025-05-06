816c5b3ca4083e3626f6c075565a7c157446a93d
M1gu3lT <miguel14032020@gmail.com>
2025-05-05 21:40:03 -0600

    actualizacion

--- src/app/shared/directives/img-broken.directive.spec.ts
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
 
 describe('ImgBrokenDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgBrokenDirective();
+  it('debería crearse la directiva', () => {
+    // Simula un elemento HTML, por ejemplo un <img>
+    const mockElement = new ElementRef(document.createElement('img'));
+    const directive = new ImgBrokenDirective(mockElement);
     expect(directive).toBeTruthy();
   });
 });
